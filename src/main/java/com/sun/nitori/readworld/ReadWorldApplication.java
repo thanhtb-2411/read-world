05d1765eeae0b426ae74b8c5b7810e6bc74224a7
tran.ba.thanh <tran.ba.thanh@sun-asterisk.com>
2022-12-12 16:22:22 +0700

    Init project

---
+++ src/main/java/com/sun/nitori/readworld/ReadWorldApplication.java
@@ -0,0 +1,14 @@
+package com.sun.nitori.readworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWorldApplication {
+
+	public static void main(String[] args) {
+		System.out.print("thanh thanh thanh");
+		SpringApplication.run(ReadWorldApplication.class, args);
+	}
+
+}
